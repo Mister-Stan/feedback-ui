52b81dee1366437a840a04cce11870c178f7c345
mister-stan <lucianstan87@yahoo.com>
2024-07-03 17:16:54 +0100

    add first commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "This is an example of a feedback item",
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "This is another example of a feedback item",
+    },
+    {
+        id: 3,
+        rating: 5,
+        text: "This is a third example of a feedback item",
+    },
+]
+
+export default FeedbackData
