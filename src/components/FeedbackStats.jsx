5fe610cd06e34b5e83c11f17154f06bc9bc6d041
mister-stan <lucianstan87@yahoo.com>
2024-07-08 17:54:25 +0100

    add spinner component

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -2,18 +2,21 @@ import React from "react"
 import { useContext } from "react"
 import FeedbackContext from "../context/FeedbackContext"
 
-
 function FeedbackStats() {
-
   const { feedback } = useContext(FeedbackContext)
 
   // Calculate ratings average
   let average =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating
-    }, 0) / feedback.length
+    feedback.length === 0
+      ? 0
+      : feedback.reduce((acc, cur) => {
+          console.log(`Current rating: ${cur.rating}, Accumulator: ${acc}`)
+          return acc + Number(cur.rating)
+        }, 0) / feedback.length
 
-  average = average.toFixed(1).replace(/[.,]0$/, "") 
+  // Format the average to one decimal place
+  average = Number(average.toFixed(1)).toString()
+
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
@@ -21,7 +24,5 @@ function FeedbackStats() {
     </div>
   )
 }
-
-
 
 export default FeedbackStats
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,30 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { v4 as uuidv4 } from "uuid"
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is feedback item 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This is feedback item 2",
-      rating: 8,
-    },
-    {
-      id: 3,
-      text: "This is feedback item 3",
-      rating: 7,
-    },
-  ])
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5001/feedback?_sort=id&_order=desc"
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Delete feedback
   const deleteFeedback = (id) => {
@@ -40,12 +39,10 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback])
   }
 
-//   Update feedback item
-  const updateFeedback = (id, updItem ) => {
+  //   Update feedback item
+  const updateFeedback = (id, updItem) => {
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
     )
   }
 
@@ -62,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
